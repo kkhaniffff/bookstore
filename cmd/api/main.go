86938ec6e95baf69c51121070deb2f8aa5c66e3f
Khanif Nursafi <kkhaniffff@gmail.com>
2026-04-29 14:23:33 +0500

    add graceful shutdown with detailed logs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/kkhaniffff/bookstore/internal/book"
 )
@@ -19,5 +26,27 @@ func main() {
 		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	go func() {
+		slog.Info("Server starting", "addr", server.Addr)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Server failed to start", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	slog.Info("Server shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server forced to shutdown", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("Server exited gracefully")
 }
